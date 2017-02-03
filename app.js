9531a9cde0c4c6bef56ff34e70ffac87362b16ba
SAM <samanthamorales8267@gmail.com>
2017-02-03 15:04:49 -0800

    deleted keith's game

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 function myFunction(){
   var userName = prompt("What\'s your name?");
+  userName = userName.toUpperCase();
   alert('Hello ' + userName + '. Thanks for coming to my site!')
 
+//Question One
   var questionOne = prompt ('So ' + userName +'. Are you new to CodeFellows?');
-  var myQuestionOne = 'Yes';
+  questionOne = questionOne.toUpperCase();
+  var myQuestionOne = 'YES';
   if (questionOne === myQuestionOne){
     alert ('Congratulations!  Welcome to Code Fellows')
   }else{
@@ -20,6 +23,8 @@ function myFunction(){
   var myHobby;
   var score = 0;
 
+
+//Question Two
   var answerTwo = prompt('What state was I born in?');
   answerTwo = answerTwo.toUpperCase();
   var myAnswerTwo = 'CALIFORNIA';
@@ -32,16 +37,20 @@ function myFunction(){
     alert('Sorry ' + userName + ', that\'s wrong. I was born in California.');
   }
 
+
+//Question Three
   var answerThree = prompt ('I love sports!  Who do you think my favorite football team is ?');
   answerThree = answerThree.toUpperCase();
   var myAnswerThree = 'SEAHAWKS';
   if (answerThree === myAnswerThree){
-    alert ('Yes!  But don\'t tell my husband!  He thinks it\'s the Patriots!')
+    alert ('Yes!  But don\'t tell my husband!  He thinks it\'s the Patriots!');
     score += 1;
   }else {
-    alert ('Nope! It\'s the Seahawks! But don\'t tell my husband!' )
+    alert ('Nope! It\'s the Seahawks! But don\'t tell my husband!' );
   }
 
+
+//Question Four
   var answerFour = prompt ('Where is my favorite restaurant to go to for happy hour?');
   answerFour = answerFour.toUpperCase();
   var myAnswerFour = 'PALISADE';
@@ -49,24 +58,30 @@ function myFunction(){
     alert ('Yes!They have the best Warm Crab Lobster Dip and American Kobe Beef Sliders!')
     score += 1;
   }else {
-    alert ('It\'s Palisade in Seattle.  They have the best Warm Crab Lobster Dip and American Kobe Beef Sliders! I recommend visiting!')
+    alert ('It\'s Palisade in Seattle.  They have the best Warm Crab Lobster Dip and American Kobe Beef Sliders! I recommend visiting!');
   }
 
-  var answerFive = prompt('How fast do I drive on the I5 freeway?');
 
+//Question Five - Logical Operator
+   var answerFive = prompt('How fast do I drive on the I5 freeway?');
 
-  if (parseInt(answerFive) >= 75) {
-    alert('I will get a ticket!');
-    score += 1;
-  }else {
-    alert('I will drive safe and have a safe trip!');
+  if (parseInt(answerFive) === 75) {
+    alert('Yes, that\'s about right!');
+
+  }else if (parseInt(answerFive) > 75){
+  alert ('Contrary to what you may think I\'m not a speed demon!');
+}else  if (parseInt(answerFive) < 75 && parseInt(answerFive) >=55){
+  alert ('My sis thinks I am still driving too slow!');}
+  else {
+    alert('What do think I am a slowpoke?');
   }
 
+
+//Question Six
   var answerSix = prompt ('How long is my name?');
 
   if ((parseInt(answerSix) * 2 / (2+2) == 4))
   {
-
     alert('You are correct!');
     score += 1;
   }
@@ -74,21 +89,29 @@ function myFunction(){
   {   alert('Nope, you got it wrong!');
 }
 
+
+//Question Seven - Array
 var answerSevenArray = prompt (' What is my favorite holiday?');
 answerSevenArray = answerSevenArray.toUpperCase();
 var myArray = ['CHRISTMAS', 'EASTER', 'CINCO DE MAYO', 'MOTHERS DAY', '4th OF JULY'];
 if (answerSevenArray === myArray[0]){
   alert ('Christmas is my favorite!  I decorate a real tree!');
   score += 1;
-}if (answerSevenArray === myArray[1]){
+}else if (answerSevenArray === myArray[1]){
   alert ('Easter is my 2nd favorite holiday!');
-}if (answerSevenArray === myArray[2]){
+
+}else if (answerSevenArray === myArray[2]){
   alert ('I\'m not much of a Cinco de Mayo fan.' );
-}if (answerSevenArray === myArray[3]){
+
+}else if (answerSevenArray === myArray[3]){
   alert('My husband makes a great Eggs Benedict brunch for Mothers Day!');
+
 }else {
   alert ('No such luck!' );
 }
+
+
+//Question Eight - Array
 var myCarArray =['SURBURBAN', 'COROLLA', 'JETTA'];
 
 var answerEightArray = prompt ('What kind of car do I drive?');
@@ -99,6 +122,8 @@ if (myCarArray.indexOf(answerEightArray)!==-1){
 }else{
   alert ('You will never guess!  Looking forward to buying a new vehicle, though!');
 }
+
+//Question Nine - Array
 var hobbies = ['SCRAPBOOKING', 'GENEALOGY', 'COACHING', 'HIKING'];
 var myHobby = prompt ('Name of a hobby of mine.');
 myHobby = myHobby.toUpperCase();
@@ -108,13 +133,30 @@ if (hobbies.indexOf(myHobby)!==-1) {
 }else{
   alert('You\'ll never guess!');
 }
-if (score === 8){
+
+for (var i = 0; i<=3; i++){
+  var myAnswer = 16;
+  var userGuess = prompt( userName + 'You have four guesses to guess my number between 1-20.');
+  if (userGuess == myAnswer){
+    alert('Of course you\'d guess that...it is Nick\'s, Maria\'s, and John\'s jersey number!');
+    score +=1;
+    break;
+  }else if (userGuess > myAnswer){
+    alert ('Too high!');
+  }else if (userGuess < myAnswer){
+    alert('Too low!');
+  }
+}
+//Score Calculation
+if (score === 9){
   alert ('Your Score is ' +score + '. You are awesome, you did perfect!');
 
 }else{
-  alert ('your score is ' + score  +' out of eight!');
+  alert ('Your score is ' + score  +' out of eight! GAMEOVER!');
 }
 }
+
+
 
 // This code below works, but I want the questions above to be the questions I
 // use to check number of answers correct or incorrect.
@@ -145,8 +187,29 @@ if (score === 8){
 // alert(message);
 // }
 //This Guess Code does not work...still working on it, having a hard time to get it to work.
-// alert (userName +', you have 4 guesses between 1-10');
-// var answer = 4;
+// alert (userName +', you have 4 guesses between 1-20');
+// var answer = 16;
+
+
+// for (var i = 0; i<=4; i++){
+//   var myAnswer = 16;
+//   var userGuess = prompt( userName + 'You have four guesses to guess my number between 1-20.');
+//   if (userGuess ===myAnswer){
+//     alert('Of course you\'d guess that...it is Nick\'s, Maria\'s, and John\'s jersey number!');
+//     score +=1;
+//     break;
+//   }else if (userGuess > myAnswer){
+//     alert ('Too high!');
+//   }else if (userGuess < myAnswer){
+//     alert('Too low!');
+//   }
+//
+
+
+
+
+
+
 // var answerNine = prompt ("Guess my number.");
 //
 // for (i=0; i<3; i++){
